36149275b5981f1e4dd3aae44491ef2634ad1dfc
chef <chef@chef.io>
2020-08-02 05:34:04 +0000

    use batch

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures symantec'
 long_description 'Installs/Configures symantec'
-version '0.2.3'
+version '0.2.4'
 chef_version '>= 14.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
